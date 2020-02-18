3ced1a637604eab1141b08bbd36df73ca792509f
LionelBeato <lionel.beato@gmail.com>
2020-02-18 08:56:18 -0500

    adds lab example

---
+++ src/MyComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MyComponent = () => {
+    return (
+        <div>
+        <h1>
+            hello
+            </h1>
+        </div>
+    )
+}
+
+export default MyComponent
